0ca05b75bff91619256da62d6e1c8abedef1bdcf
zhousy612 <zhousy27@chinatelecom.cn>
2026-01-19 00:48:47 +0800

    add bash

---
+++ bash/noniid_resnet.sh
@@ -0,0 +1,5 @@
+python run.py --cfg configs/example.yaml noniid.alpha 0.1
+python run.py --cfg configs/example.yaml noniid.alpha 0.5
+python run.py --cfg configs/example.yaml noniid.alpha 1.0
+python run.py --cfg configs/example.yaml noniid.alpha 5.0
+python run.py --cfg configs/example.yaml noniid.alpha 10.0
