8741bafc10d8ebce9c15eab9a29ec4754d097cf2
zhousy612 <zhousy27@chinatelecom.cn>
2026-01-19 01:12:51 +0800

    fix bug

--- bash/noniid_resnet.sh
+++ bash/noniid_resnet.sh
@@ -1,5 +1,5 @@
-python /root/fl_comression_gpt/run.py --cfg configs/example.yaml noniid.alpha 0.1
-python /root/fl_comression_gpt/run.py --cfg configs/example.yaml noniid.alpha 0.5
-python /root/fl_comression_gpt/run.py --cfg configs/example.yaml noniid.alpha 1.0
-python /root/fl_comression_gpt/run.py --cfg configs/example.yaml noniid.alpha 5.0
-python /root/fl_comression_gpt/run.py --cfg configs/example.yaml noniid.alpha 10.0
+python /root/fl_comression_gpt/run.py --cfg /root/fl_comression_gpt/configs/example.yaml noniid.alpha 0.1
+python /root/fl_comression_gpt/run.py --cfg /root/fl_comression_gpt/configs/example.yaml noniid.alpha 0.5
+python /root/fl_comression_gpt/run.py --cfg /root/fl_comression_gpt/configs/example.yaml noniid.alpha 1.0
+python /root/fl_comression_gpt/run.py --cfg /root/fl_comression_gpt/configs/example.yaml noniid.alpha 5.0
+python /root/fl_comression_gpt/run.py --cfg /root/fl_comression_gpt/configs/example.yaml noniid.alpha 10.0
