86031ae1affcea3fe25cc8a780bc96b7799b313e
zhousy612 <zhousy27@chinatelecom.cn>
2026-01-19 00:51:04 +0800

    update bash path

--- bash/noniid_resnet.sh
+++ bash/noniid_resnet.sh
@@ -1,5 +1,5 @@
-python run.py --cfg configs/example.yaml noniid.alpha 0.1
-python run.py --cfg configs/example.yaml noniid.alpha 0.5
-python run.py --cfg configs/example.yaml noniid.alpha 1.0
-python run.py --cfg configs/example.yaml noniid.alpha 5.0
-python run.py --cfg configs/example.yaml noniid.alpha 10.0
+python ~/fl_comression_gpt/run.py --cfg configs/example.yaml noniid.alpha 0.1
+python ~/fl_comression_gpt/run.py --cfg configs/example.yaml noniid.alpha 0.5
+python ~/fl_comression_gpt/run.py --cfg configs/example.yaml noniid.alpha 1.0
+python ~/fl_comression_gpt/run.py --cfg configs/example.yaml noniid.alpha 5.0
+python ~/fl_comression_gpt/run.py --cfg configs/example.yaml noniid.alpha 10.0
